bf88a534f3b7be9f2dd22e600c84174fa69746bb
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-06-16 18:02:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
+// Create instances and call methods
+var person = new Person("John", 25);
+var employee = new Employee("Alice", 30, "Manager");
+
+person.greet();
+employee.greet();
+employee.jobGreet();
+
+// Display output
+var outputDiv = document.getElementById('output');
+outputDiv.innerHTML = `
+  <p>Person:</p>
+  <p>Name: ${person.name}</p>
+  <p>Age: ${person.age}</p>
+  <br>
+  <p>Employee:</p>
+  <p>Name: ${employee.name}</p>
+  <p>Age: ${employee.age}</p>
+  <p>Job Title: ${employee.jobTitle}</p>
+`;
